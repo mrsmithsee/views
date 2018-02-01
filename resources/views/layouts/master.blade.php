5cfa50f37eb3bc6c6a6914127289be450c66d97d
php 17 <php.17@dino.cec.ime.usp.br>
2018-01-31 22:19:08 -0200

    acrescentando index e show cursos

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Página</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <h1>Cabeçalho</h1>
+      </div>
+
+        @yield('conteudo')
+
+      <div class="footer">
+        <h2>Rodapé</h2>
+    </div>
+    <script type="text/javascript">
+
+    </script>
+  </body>
+</html>
